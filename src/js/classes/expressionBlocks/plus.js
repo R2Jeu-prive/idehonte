336a4eb396aa88fbbefa0d277c6375ca7e1f5213
Pierre <R2Jeu.prive@gmail.com>
2024-06-21 17:01:50 +0200

    fixed types checked

--- src/js/classes/expressionBlocks/plus.js
+++ src/js/classes/expressionBlocks/plus.js
@@ -18,13 +18,13 @@ class PlusEB extends ExpressionBlock{
         if(this.childrenBlocks[0] != null){
             if(!(this.childrenBlocks[0] instanceof ExpressionBlock)){return false;} // not an expression
             if(!this.childrenBlocks[0].CheckValid()){return false;} // children not valid
-            let [typeValid, _1, _2] = this.childrenBlocks[0].GetEvalType().CheckCompatibilityWith(new ExpressionTypeConstr([], ConstrType.Get("unit")));
+            let [typeValid, _1, _2] = this.childrenBlocks[0].GetEvalType().CheckCompatibilityWith(new ExpressionTypeConstr([], ConstrType.Get("int")));
             if(!typeValid){return false;} // invalid type
         }
         if(this.childrenBlocks[1] != null){
             if(!(this.childrenBlocks[1] instanceof ExpressionBlock)){return false;} // not an expression
             if(!this.childrenBlocks[1].CheckValid()){return false;} // children not valid
-            let [typeValid, _1, _2] = this.childrenBlocks[1].GetEvalType().CheckCompatibilityWith(new ExpressionTypeConstr([], ConstrType.Get("unit")));
+            let [typeValid, _1, _2] = this.childrenBlocks[1].GetEvalType().CheckCompatibilityWith(new ExpressionTypeConstr([], ConstrType.Get("int")));
             if(!typeValid){return false;} // invalid type
         }
         return true;
