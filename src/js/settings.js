0ba5a70681093ebb561cd4da708a24ab49648241
EliottPiccand <github.excavator730@passmail.net>
2024-07-13 13:16:59 +0200

    themes

--- src/js/settings.js
+++ src/js/settings.js
@@ -5,9 +5,27 @@ function settingsLoader() {
     settingsButton.addEventListener("click", (event) => {
         settingsMenu.style.display = settingsMenu.style.display == "flex" ? "none" : "flex";
     });
+
+    document.addEventListener("keydown", (event) => {
+        console.log(event.key);
+    });
+
+    if (localStorage.getItem("ide-theme") == null) {
+        localStorage.setItem("ide-theme", "default");
+    } else {
+        setTheme(localStorage.getItem("ide-theme"))
+    }
 }
 settingsLoader();
 
-function setTheme() {
-    const theme = document.getElementById("theme").value;
+function setTheme(theme) {
+
+    if (theme == null) {
+        theme = document.getElementById("theme-change").value;
+        localStorage.setItem("ide-theme", theme);
+    }
+
+    const linkTag = document.getElementById("theme");
+    linkTag.setAttribute("href", `css/themes/${theme}.css`)
+    console.log(`Theme switched to ${theme}`);
 }
